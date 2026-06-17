f9b5b25c2498f6b239974f401d26311c6daca00c
Francisco José Molina Blanco <caetin5@gmail.com>
2026-06-17 15:50:08 +0200

    Cameras Transitions configured

--- Assets/Scripts/Player/PlayerCameraController.cs
+++ Assets/Scripts/Player/PlayerCameraController.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
-
+using Cinemachine;
 public class PlayerCameraController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    
+    float normalFov = 45f;
+    float freezeFov = 85f;
+    float targetFov;
+
+    float fovTransitionSpeed = 2f;
+
+    [SerializeField]  CinemachineVirtualCamera myCamera;
+
+    private PlayerTimeFreeze timeFreeze;
+
+
+
+    void Awake()
     {
-        
+        timeFreeze = GetComponent<PlayerTimeFreeze >();
+	    targetFov = normalFov;
+
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (timeFreeze.IsFrozen)
+        {
+            targetFov = freezeFov;
+        }
+        else
+        {
+            targetFov = normalFov;
+        }
+        CambioCamara();
     }
+
+    void CambioCamara()
+    {
+        myCamera.m_Lens.FieldOfView = Mathf.Lerp
+        (myCamera.m_Lens.FieldOfView, targetFov, fovTransitionSpeed * Time.unscaledDeltaTime);
+    }
+
 }
